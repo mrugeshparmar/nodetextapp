265abf299d7157d1dac924ce1c2682c6d7708524
mjparmar <mrpa4248@gmail.com>
2018-01-10 17:23:06 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,77 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const Nexmo = require('nexmo');
+const socketio = require('socket.io');
+
+//Init Nexmo
+
+const nexmo = new Nexmo({
+    apiKey: '55819f0a',
+    apiSecret: 'fd46423bf628f9e7'
+}, {debug: true});
+
+//Init the app
+const app = express();
+
+//Template engine setup
+app.set('view engine','html');
+app.engine('html',ejs.renderFile);
+
+//Public folder setup
+app.use(express.static(__dirname + '/public'));
+
+//body parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
+
+//Index Route
+app.get('/', (req, res)=>{
+    res.render('index');
+});
+
+//catch form submit
+app.post('/', (req,res) =>{
+    //res.send(req.body);
+    //console.log(req.body);
+    const number = req.body.number;
+    const text = req.body.text;
+
+    nexmo.message.sendSms(
+        '12132055761', number, text, {type:'unicode'},
+    (err, responseData)=> {
+        if(err){
+            console.log(err);
+        } else {
+            console.dir(responseData);
+            //Get data from response
+
+            const data = {
+                id: responseData.messages[0]['message-id'],
+                number : responseData.messages[0]['to']
+            }
+
+            //Emit to the client
+            io.emit('smsStatus', data);
+
+        }
+     }
+    );
+});
+
+//define port
+const port = 3000;
+
+//start server
+const server = app.listen(port, ()=> console.log(`Server started on port ${port}`));
+
+
+//Connect to socket.io
+const io = socketio(server);
+io.on('connection', (socket) => {
+    console.log('Connected');
+    io.on('disconnect', () =>{
+        console.log('Disconnected');
+    })
+
+})
